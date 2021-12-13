dd2c0aa3468da4a15fb88f00df4db195fab3a4db
makerboomRijnland <makerboom@mborijnland.nl>
2021-12-13 15:23:56 +0100

    Uitwerking

--- js/clock.js
+++ js/clock.js
@@ -10,6 +10,30 @@
     RESULTAAT: 88:88:88
 */
 
+function startClock() {
+    testClock();
+    setInterval(drawClock, 1000);
+}
+
+window.addEventListener("load", startClock);
+
+function testClock() {
+    document.getElementById("-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+}
+
 /* 
     Stap 2.
 
@@ -25,9 +49,9 @@
     RESULTAAT: 88:88:88
 */
 
-/*
-    
-*/
+function testDigit(digitId) {
+    drawDigit(digitId, 9);
+}
 
 /*
     Stap 3.
@@ -38,6 +62,138 @@
         - Herschrijf testDigit(digitId) zodat deze de functie drawDigit gebruikt.
 */
 
+function drawDigit(digitId, digit) {
+    switch (digit) {
+        case 0:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 1:
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 2:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-3").style.display =
+                "block";
+            break;
+
+        case 3:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 4:
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 5:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 6:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 7:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 8:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+
+        case 9:
+            document.getElementById(digitId + "-hbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-hbar-3").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-1").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-2").style.display =
+                "block";
+            document.getElementById(digitId + "-vbar-4").style.display =
+                "block";
+            break;
+    }
+}
+
 /*
     Stap 4.
     
@@ -45,6 +201,13 @@
         - Deze functie moet alle 'bars' uitzetten (.style.display='none').
 */
 
+function resetClock() {
+    let bars = document.getElementsByClassName('bar');
+
+    for( let index = 0; index < bars.length ; index++) {
+        bars[index].style.display = 'none';
+    }
+}
 
 /*
     Stap 5.
@@ -61,6 +224,45 @@
 
 */
 
+function drawClock() {
+    resetClock();
+
+    let now = new Date(); // Het huidige tijdstip opvragen
+    let seconds = now.getSeconds(); // De seconden opvragen
+    let secondsDigits = digitsOf(seconds); // Maakt een array van getallen van de seconden.
+
+    if(seconds < 10) {
+        drawDigit('second-1', 0);
+        drawDigit('second-2', secondsDigits[0]);
+    } else {
+        drawDigit('second-1', secondsDigits[0]);
+        drawDigit('second-2', secondsDigits[1]);
+    }
+
+    let minutes = now.getMinutes(); // De seconden opvragen
+    let minutesDigits = digitsOf(minutes); // Maakt een array van getallen van de seconden.
+
+    if(minutes < 10) {
+        drawDigit('minute-1', 0);
+        drawDigit('minutes-2', minutesDigits[0]);
+    } else {
+        drawDigit('minutes-1', minutesDigits[0]);
+        drawDigit('minutes-2', minutesDigits[1]);
+    }
+
+    let hours = now.getHours(); // De seconden opvragen
+    let hoursDigits = digitsOf(hours); // Maakt een array van getallen van de seconden.
+
+    if(hours < 10) {
+        drawDigit('hour-1', 0);
+        drawDigit('hour-2', hoursDigits[0]);
+    } else {
+        drawDigit('hour-1', hoursDigits[0]);
+        drawDigit('hour-2', hoursDigits[1]);
+    }
+}
+
+
 /*
     Stap 6.
     
@@ -76,21 +278,20 @@
 
 */
 
-
 /*
     Stap 8
 
     Laat de seperators knipperen (1s aan / 1s uit).
 */
 
-
-
 /**
  * Returns an Array of digits of the given Number
  * @param {Number} number Any number
  * @returns Array<Number>
  */
- function digitsOf(number) {
-    return number.toString().split('').map((digit) => parseInt(digit));
+function digitsOf(number) {
+    return number
+        .toString()
+        .split("")
+        .map((digit) => parseInt(digit));
 }
-
